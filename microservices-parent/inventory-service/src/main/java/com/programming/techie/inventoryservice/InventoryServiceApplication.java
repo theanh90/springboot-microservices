4f2112ba4dd495aa4cd9970d37ce14ce51046b17
Anh Bui <anh.buithe@axonactive.com>
2024-01-09 13:59:30 +0700

    Add parent service

---
+++ microservices-parent/inventory-service/src/main/java/com/programming/techie/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,37 @@
+package com.programming.techie.inventoryservice;
+
+import com.programming.techie.inventoryservice.model.Inventory;
+import com.programming.techie.inventoryservice.repository.InventoryRepository;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@Slf4j
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = Inventory.builder()
+					.skuCode("iphone_13")
+					.quantity(100)
+					.build();
+
+			Inventory inventory1 = Inventory.builder()
+					.skuCode("iphone_13_red")
+					.quantity(0)
+					.build();
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+			log.info("init database for inventory");
+		};
+	}
+}
