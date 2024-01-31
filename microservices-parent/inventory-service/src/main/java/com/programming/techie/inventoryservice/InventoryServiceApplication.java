f40cf55868196a333b702727652b1b10d3c01676
Anh Bui The <anh.buithe@axonactive.com>
2024-01-31 12:35:59 +0700

    Setup Discovery server (Netfix Eureka)

--- microservices-parent/inventory-service/src/main/java/com/programming/techie/inventoryservice/InventoryServiceApplication.java
+++ microservices-parent/inventory-service/src/main/java/com/programming/techie/inventoryservice/InventoryServiceApplication.java
@@ -2,14 +2,15 @@ package com.programming.techie.inventoryservice;
 
 import com.programming.techie.inventoryservice.model.Inventory;
 import com.programming.techie.inventoryservice.repository.InventoryRepository;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Slf4j
 public class InventoryServiceApplication {
 
--- microservices-parent/order-service/src/main/java/com/programming/techie/orderservice/service/OrderService.java
+++ microservices-parent/order-service/src/main/java/com/programming/techie/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -37,8 +37,8 @@ public class OrderService {
         List<String> skuCodes = orderRequest.getOrderLineItemsDtoList().stream()
                 .map(OrderLineItemsDto::getSkuCode)
                 .toList();
-        InventoryResponse[] result = webClient.get()
-                .uri("http:localhost:8082/api/inventoryservice", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+        InventoryResponse[] result = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
